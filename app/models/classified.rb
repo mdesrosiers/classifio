85d8da93f142a8d0f35e1d02099b786593e50ec0
Martin Desrosiers <martin.desrosiers@gmail.com>
2012-06-30 20:43:44 -0700

    Whitelist Classified model JSON params

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -1,4 +1,8 @@
 class Classified < ActiveRecord::Base
   attr_accessible :title, :description
   validates_presence_of :title, :description
+
+  def as_json(options = {})
+    super(options.merge(only: [:id, :title, :description]))
+  end
 end
