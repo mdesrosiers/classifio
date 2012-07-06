f053d9259b6b7b90297f14e1fd1911f179527565
Martin Desrosiers <martin.desrosiers@gmail.com>
2012-07-06 01:13:01 -0700

    Add missing thumbnail image fixture

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-Classified.create(title: 'Whitecaps Tickets', description: 'Section 46, row C, seats 1 and 2')
-Classified.create(title: 'Bike Trailer', description: 'Chariot Cougar 2, in good condition')
-Classified.create(title: 'iPhone 4', description: 'iPhone 4, 32G, white, no scratches')
-Classified.create(title: 'Mountain Bike', description: 'Brodie Bruzza 2012')
-Classified.create(title: 'Star Wars Blu-ray', description: 'The complete sage, 6 movies')
+missing_thumbnail = File.open(Rails.root.join('test', 'fixtures', 'missing_thumbnail.png'))
+
+Classified.create(title: 'Whitecaps Tickets', description: 'Section 46, row C, seats 1 and 2', image: missing_thumbnail)
+Classified.create(title: 'Bike Trailer', description: 'Chariot Cougar 2, in good condition', image: missing_thumbnail)
+Classified.create(title: 'iPhone 4', description: 'iPhone 4, 32G, white, no scratches', image: missing_thumbnail)
+Classified.create(title: 'Mountain Bike', description: 'Brodie Bruzza 2012', image: missing_thumbnail)
+Classified.create(title: 'Star Wars Blu-ray', description: 'The complete sage, 6 movies', image: missing_thumbnail)
