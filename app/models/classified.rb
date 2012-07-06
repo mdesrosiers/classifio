5ec582a7e82fdececa5ef2aae01c18a4c261bf0a
Martin Desrosiers <martin.desrosiers@gmail.com>
2012-07-06 00:48:32 -0700

    Fix thumbnail size

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -2,13 +2,13 @@ class Classified < ActiveRecord::Base
   attr_accessible :title, :description, :image
   validates_presence_of :title, :description
 
-  has_attached_file :image, default_url: 'http://placehold.it/360x268'
+  has_attached_file :image, default_url: 'http://placehold.it/360x268.png', :styles => { :thumbnail => "360x268>" }
 
   def as_json(options = {})
-    super(options.merge(only: [:id, :title, :description], methods: :image_url))
+    super(options.merge(only: [:id, :title, :description], methods: :thumbnail_url))
   end
 
-  def image_url
-    image.url
+  def thumbnail_url
+    image.url(:thumbnail)
   end
 end
