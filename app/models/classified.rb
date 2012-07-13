01dbac14d1561dfa1debbe1b47507a387a8d1c16
Martin Desrosiers <martin.desrosiers@gmail.com>
2012-07-12 21:43:29 -0700

    Only include image in json if present

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -5,10 +5,14 @@ class Classified < ActiveRecord::Base
   has_attached_file :image, default_url: 'http://placehold.it/360x268.png', :styles => { :thumbnail => "360x268>" }
 
   def as_json(options = {})
-    super(options.merge(only: [:id, :title, :description], methods: :thumbnail_url))
+    super(options.merge(only: [:id, :title, :description], methods: json_methods))
   end
 
   def thumbnail_url
     image.url(:thumbnail)
+  end
+
+  def json_methods
+    image.present? ? [:thumbnail_url] : []
   end
 end
